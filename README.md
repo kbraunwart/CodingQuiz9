8740d859a85587d813277e82f9d31456daae144d
kbraunwart <kbraunwart@clarku.edu>
2023-12-08 12:21:34 -0500

    miniMaxSum

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # CodingQuiz9
 
-Time Complexity:
+Mini Max Sum Time Complexity:
+O(n) as a for loop is needed to iterate through each integer in the array
 
-Space Complexity:
+Mini Max Sum Space Complexity:
+O(c) as a constant number of variables are used to hold
+long and int values throughout the code.
 
 Time Complexity:
 
---
+++ miniMaxSum.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+        // Write your code here
+        long min = arr.get(0);
+        long max = arr.get(0);
+        long sum = arr.get(0);
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr.get(i) < min) {
+                min = arr.get(i);
+            } else if (arr.get(i) > max) {
+                max = arr.get(i);
+            }
+            sum += arr.get(i);
+        }
+        long sumMinusMax = sum - max;
+        long sumMinusMin = sum - min;
+
+        System.out.println(sumMinusMax + " " + sumMinusMin);
+
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
